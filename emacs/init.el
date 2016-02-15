39e7b8133664e029f24635b45dc6e25b9d12b298
Daniel Strunk <me@dstrunk.com>
2016-02-14 23:28:36 -0500

    add settings to init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load-file "~/.emacs.d/sensible-defaults.el")
 (sensible-defaults/use-all-settings)
 (sensible-defaults/use-all-keybindings)
@@ -5,4 +12,5 @@
 (defun dstrunk/configure-cask ()
   (require 'cask "~/.cask/cask.el")
   (cask-initialize)
-  (require 'pallet))
+  (require 'pallet)
+  (pallet-mode t))
