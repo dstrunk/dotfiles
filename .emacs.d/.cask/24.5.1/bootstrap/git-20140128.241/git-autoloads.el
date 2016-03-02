1f065227f50325f89fbb7a8dd6a7a34af53797ff
Daniel Strunk <me@dstrunk.com>
2016-03-01 20:39:51 -0500

    updates; allow dotfiles to be installed on linux

---
+++ .emacs.d/.cask/24.5.1/bootstrap/git-20140128.241/git-autoloads.el
@@ -0,0 +1,15 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("git.el") (22230 12334 55725 700000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-autoloads.el ends here
