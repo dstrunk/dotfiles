76dd535a20d15dcdd97f70e9c7b1c1486cc21307
Daniel Strunk <me@dstrunk.com>
2016-05-09 17:36:36 -0400

    updates

--- emacs/init.el
+++ emacs/init.el
@@ -1,1 +1,13 @@
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org/books.org" "~/org/todo.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
