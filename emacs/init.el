35671a96a106ffd17ae797ee78141b3fdb84ffb9
Daniel Strunk <me@dstrunk.com>
2016-07-09 10:31:53 -0400

    Add Rails config for emacs, general changes
    Update emacs config for Rails changes, magit column wrapping, and more.

--- emacs/init.el
+++ emacs/init.el
@@ -4,7 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/org/books.org" "~/org/todo.org"))))
+ '(org-agenda-files (quote ("~/Dropbox/org/books.org" "~/org/todo.org")))
+ '(safe-local-variable-values
+   (quote
+    ((flyspell-mode 1)
+     (olivetti-mode)
+     (linum-mode -1)
+     (org-mode t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
